b6a9d5b2d9a40545ac3f14dc35d75ccb8ca243ba
Ananya1812 <ananyakini04@gmail.com>
2024-01-31 17:12:44 +0530

    updated

---
+++ 39-Popup-form/app.js
@@ -0,0 +1,8 @@
+document.getElementById("openForm").addEventListener("click", function() {
+    document.getElementById("myForm").style.display = "flex";
+  });
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
