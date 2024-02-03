750c6cadd699fa1da8596212ea37d54c8dcee7c1
Ananya1812 <ananyakini04@gmail.com>
2024-02-03 10:37:40 +0530

    updated

---
+++ 20-Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ea8f364a-080d-4c9d-900f-1a97a80768f9';
+export const MODAL_CLOSE_SEC = 2.5;
